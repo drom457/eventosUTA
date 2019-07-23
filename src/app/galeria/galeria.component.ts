207f61e07e56a3ce547ce69c6756501153eeaeaf
Bryan <Fol_brn_@hotmail.com>
2019-07-23 16:28:21 -0500

    Update galeria.component.ts

--- src/app/galeria/galeria.component.ts
+++ src/app/galeria/galeria.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MaxLengthValidator } from '@angular/forms';
+
+declare var require: any
 
 @Component({
   selector: 'app-galeria',
@@ -6,14 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./galeria.component.css']
 })
 export class GaleriaComponent implements OnInit {
+  imagenes: Array<any> = []
 
-  constructor() { 
+  constructor() {
+    for (var i = 1; i < 18; i++) {
+      const path = 'assets/img/Imagen' + i + '.jpg';
+      if (this.UrlExists(path)) {
+        this.imagenes.push('Imagen' + i + '.jpg');
+      } else {
+        
+      }
+    }
 
-    
   }
+
+  UrlExists(url) {
+    if (url) {
+      var req = new XMLHttpRequest();
+      req.open('HEAD', url, false);
+      req.send();
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  //sudo npm install fs
+  //npm install express
 
   ngOnInit() {
   }
-  
 
 }
